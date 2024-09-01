4b0d4a25c65535bfe08acdfaf514d7013f1fa4c0
danimohli <danimohlife@gmail.com>
2024-09-01 10:55:00 +0100

    infinity_add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+
+    total = 0
+
+    for x in range((len(argv) - 1)):
+
+        total += int(argv[1 + x])
+    print(total)
+
+
