9bc56c29bc037cdab7628e8ca5feba1b31e5a01b
danimohli <danimohlife@gmail.com>
2024-09-04 16:01:49 +0100

    with_size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defining class Square Module
+"""
+
+
+class Square:
+
+    """
+    Declaration of class Square with
+
+    private variable size
+    """
+    def __init__(self, size):
+        __size = size
