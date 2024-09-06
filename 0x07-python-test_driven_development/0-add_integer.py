cf1ab2b2ba64f973f680deaba6fea12ee7b14e36
danimohli <danimohlife@gmail.com>
2024-09-06 16:13:51 +0100

    Integers addition space rm

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,10 +18,10 @@ def add_integer(a, b=98):
     Returns:
         int: The sum of the two numbers, converted to an integer.
     """
-    
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
