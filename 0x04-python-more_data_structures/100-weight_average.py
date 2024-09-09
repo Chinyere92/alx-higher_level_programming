53cb9f3e7dfcbcaa6954eb074c7906e56417d87c
danimohli <danimohlife@gmail.com>
2024-09-09 13:10:57 +0100

    weight

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,6 +5,9 @@ def weight_average(my_list=[]):
     to_score = 0
     pep = 0
 
+    if len(my_list) == 0:
+        return 0
+
     for st in my_list:
         to_score += (st[0] * st[1])
         pep += st[1]
