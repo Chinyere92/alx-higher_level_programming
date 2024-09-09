5e8ed686f2093c5652edd4ddb4e70e8ec260b8c6
danimohli <danimohlife@gmail.com>
2024-09-09 12:34:56 +0100

    roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    rmn = 0
+
+    if roman_string is None or not isinstance(roman_string, str):
+        return rmn
+
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    prev = roman_string[0]
+
+    for curr in roman_string:
+
+        if roman[curr] == roman[prev]:
+            rmn += roman[curr]
+            prev = curr
+        elif roman[curr] > roman[prev]:
+            rmn = (roman[curr] - rmn) - rmn
+        else:
+            rmn += roman[curr]
+    return rmn
