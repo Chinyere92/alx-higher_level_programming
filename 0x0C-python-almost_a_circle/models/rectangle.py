46078fcbc2549d68ee536cacb99141319bf0bf01
danimohli <danimohlife@gmail.com>
2024-09-13 12:36:16 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,16 +101,12 @@ class Rectangle(Base):
     def display(self):
         """
         Prints the rectangle using the character '#',
-        taking into account the 'x' and 'y' offsets 
+        taking into account the 'x' and 'y' offsets
         for horizontal and vertical positioning.
         """
         print("\n" * self.__y, end='')
         for _ in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
-
-        for _ in range(self.__height):
-
-            print('#' * self.__width)
 
     def __str__(self):
         """
